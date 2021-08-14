27c5b24763307aa8a79b21af3718daee6b25fb56
johnforjc <animeloverz072@gmail.com>
2021-08-14 21:45:37 +0700

    styling night mode info-cuaca

--- src/script/component/cityItem.js
+++ src/script/component/cityItem.js
@@ -20,6 +20,15 @@ class CityItem extends HTMLElement {
         this._kelembaban = responseJson.main.humidity;
         this._icon = `http://openweathermap.org/img/wn/${responseJson.weather[0].icon}.png`;
 
+        // ambil timezone lokasi dari api
+        this._timezone = responseJson.timezone / 3600;
+
+        // ambil waktu lokal(komputer client) lalu ditambah dengan perbedaan waktunya
+        this._date = new Date();
+        this._timezoneDiff = this._date.getTimezoneOffset() / 60 + this._timezone;
+
+        this._hour = (this._date.getHours() + this._timezoneDiff) % 24;
+
         this.render();
       })
       .catch((error) => this.renderError());
@@ -27,15 +36,41 @@ class CityItem extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <h2>${this._city.name}</h2>
-      <div class="delete-btn" id="delete-btn" data-city="${this._city.name}">X</div>
-      <img src="${this._icon}" />
-      <h3>${this._cuaca}</h3>
-      <h3>${this._suhu}</h3>
-      <h3>${this._kelembaban}</h3>
+      <div class="info-header">
+        <h2>${this._city.name}</h2>
+        <div class="delete-btn" id="delete-btn" data-city="${this._city.name}">X</div>
+      </div>
+
+      <div class="info-body">
+        <img class="icon-cuaca" src="${this._icon}" />
+        <h3 class="info-cuaca">${this._cuaca}</h3>
+        <div class="detail-info-box">
+          <div class="detail-info">
+            <div class="info-title">
+              Temperature
+            </div>
+            <div class="info-context">
+              ${this._suhu}
+            </div>
+          </div>
+          <div class="detail-info">
+            <div class="info-title">
+              Kelembaban
+            </div>
+            <div class="info-context">
+              ${this._kelembaban}
+            </div>
+          </div>
+        </div>
+      </div>
     `;
 
     this.querySelector("#delete-btn").addEventListener("click", this._deleteEvent);
+    if (this._hour >= 6 && this._hour <= 18) {
+      this.classList.add("siang");
+    } else {
+      this.classList.add("malam");
+    }
   }
 
   renderError() {
